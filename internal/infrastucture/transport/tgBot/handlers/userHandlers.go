71929ad91cf5954ff865e2905873e9d7acdd3f78
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-11-29 19:28:41 +0300

    added saveWord dto and handler

--- internal/infrastucture/transport/tgBot/handlers/userHandlers.go
+++ internal/infrastucture/transport/tgBot/handlers/userHandlers.go
@@ -5,6 +5,6 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 func (h *Handlers) StartCommand(update tgbotapi.Update) string {
 	// REMOVE: for testing
 	//ADD: registration logic and greeting msg
-	msg := "Hello, this is LANGBRV bot: bot for learning English"
+	msg := "Привет, это LANGBRV, бот для изучения английского языка! Напиши /add_word чтобы добавить первое слово!"
 	return msg
 }
