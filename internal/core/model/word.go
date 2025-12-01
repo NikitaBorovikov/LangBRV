a130696c4d35db7a169076bbf3a7024146b0f457
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-12-01 20:47:52 +0300

    added gorm indexes

--- internal/core/model/word.go
+++ internal/core/model/word.go
@@ -4,9 +4,9 @@ import "time"
 
 type Word struct {
 	ID          string    `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	UserID      int64     `gorm:"not null"`
-	Original    string    `gorm:"type:varchar(500);not null"`
+	UserID      int64     `gorm:"not null;index:idx_user_last_seen"`
+	Original    string    `gorm:"type:varchar(500);not null;index:idx_user_original"`
 	Translation string    `gorm:"type:varchar(500);not null"`
-	LastSeen    time.Time `gorm:"not null"`
+	LastSeen    time.Time `gorm:"not null;index:idx_user_last_seen"`
 	CreatedAt   time.Time `gorm:"autoCreateTime"`
 }
