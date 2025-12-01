a12aa4dc01dca6680a325bd928b6764d0cd8f14a
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-12-01 09:38:50 +0300

    added GORM tegs

--- internal/core/model/word.go
+++ internal/core/model/word.go
@@ -3,9 +3,10 @@ package model
 import "time"
 
 type Word struct {
-	ID          string
-	UserID      int64
-	UserWord    string
-	Translation string
-	CreatedAt   time.Time
+	ID          string    `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	UserID      int64     `gorm:"not null"`
+	Original    string    `gorm:"type:varchar(500);not null"`
+	Translation string    `gorm:"type:varchar(500);not null"`
+	LastSeen    time.Time `gorm:"not null"`
+	CreatedAt   time.Time `gorm:"autoCreateTime"`
 }
