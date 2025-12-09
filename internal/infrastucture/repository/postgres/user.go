8a0208361cccd5d65359fbbfe33c0e2616ed8383
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-12-09 20:34:17 +0300

    chnaged postgres repository methods (gorm -> sqlx)

--- internal/infrastucture/repository/postgres/user.go
+++ internal/infrastucture/repository/postgres/user.go
@@ -3,25 +3,35 @@ package postgres
 import (
 	"langbrv/internal/core/model"
 
-	"gorm.io/gorm"
+	"github.com/jmoiron/sqlx"
 )
 
 type UserRepo struct {
-	db *gorm.DB
+	db *sqlx.DB
 }
 
-func NewUserRepo(db *gorm.DB) *UserRepo {
+func NewUserRepo(db *sqlx.DB) *UserRepo {
 	return &UserRepo{
 		db: db,
 	}
 }
 
 func (r *UserRepo) CreateOrUpdate(user *model.User) error {
-	return r.db.Save(user).Error
+	query := `
+		INSERT INTO users (id, username, created_at) 
+		VALUES (:id, :username, :created_at)
+		ON CONFLICT (id) DO UPDATE 
+		SET created_at = EXCLUDED.created_at
+	`
+	_, err := r.db.NamedQuery(query, user)
+	return err
 }
 
 func (r *UserRepo) GetByID(userID string) (*model.User, error) {
 	var user model.User
-	result := r.db.Where("id = ?", userID).Find(&user)
-	return &user, result.Error
+
+	query := `SELECT * FROM users WHERE id = $1`
+
+	err := r.db.Get(&user, query, userID)
+	return &user, err
 }
