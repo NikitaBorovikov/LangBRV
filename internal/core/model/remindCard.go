2875e25d1503e7995cbf79e4b4f0d84979e7c9ab
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2025-12-22 19:01:35 +0300

    feature: add remindCard model

---
+++ internal/core/model/remindCard.go
@@ -0,0 +1,41 @@
+package model
+
+type RemindCardStatus string
+
+const (
+	SingleCard RemindCardStatus = "SINGLE"
+	FirstCard  RemindCardStatus = "FIRST"
+	MiddleCard RemindCardStatus = "MIDDLE"
+	LastCard   RemindCardStatus = "LAST"
+
+	DefaultCardNumber int = 1
+)
+
+type RemindCard struct {
+	UserID      int64
+	Status      RemindCardStatus
+	CurrentCard int
+	TotalCards  int
+	RemindMsgID int
+	Words       []Word
+}
+
+func NewRemindCard(userID int64) *RemindCard {
+	return &RemindCard{
+		UserID:      userID,
+		CurrentCard: DefaultCardNumber,
+	}
+}
+
+func (c *RemindCard) DetermineStatus() {
+	switch {
+	case c.CurrentCard == 1 && c.TotalCards == 1:
+		c.Status = SingleCard
+	case c.CurrentCard == 1 && c.TotalCards > 1:
+		c.Status = FirstCard
+	case c.CurrentCard != 1 && c.CurrentCard == c.TotalCards:
+		c.Status = LastCard
+	default:
+		c.Status = MiddleCard
+	}
+}
